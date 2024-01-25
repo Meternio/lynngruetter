f3e602ff999e4c9ae194efb054e95dad2119929e
Florian Hitz <web.florianhitz@gmail.com>
2024-01-25 10:02:57 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: '',
+      },
+      bridge: {
+        customParent: 'https://app.storyblok.com',
+      },
+      components: {
+        page: 'storyblok/Page',
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
