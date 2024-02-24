85c4662e809e8bf9f67f45216cb387641e43e6ab
Florian Hitz <web.florianhitz@gmail.com>
2024-02-24 15:35:20 +0100

    wip

--- src/pages/[...slug].js
+++ src/pages/[...slug].js
@@ -1,3 +1,7 @@
 export async function GET({ params, redirect }) {
+  // exception robots.txt
+  if (params.slug[0] === "robots.txt") {
+    return { body: "User-agent: *\nDisallow: /" };
+  }
   return redirect("/", 301);
 }
