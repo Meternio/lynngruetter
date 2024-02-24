1cfb4affd5283dd97caab937b5ae448f956bf0d3
Florian Hitz <florian@raffiniert.biz>
2024-02-24 17:12:57 +0100

    favicon

--- src/pages/[...slug].js
+++ src/pages/[...slug].js
@@ -1,7 +1,3 @@
 export async function GET({ params, redirect }) {
-  // exception robots.txt
-  if (params.slug[0] === "robots.txt") {
-    return { body: "User-agent: *\nDisallow: /" };
-  }
   return redirect("/", 301);
 }
